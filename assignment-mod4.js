bca24c9eec16316397d7996bbf0d66b7e7d85d2e
Alex Pandi <ppandalex@hotmail.com>
2023-04-26 20:43:05 +0100

    assignment done

--- assignment-mod4.js
+++ assignment-mod4.js
@@ -13,16 +13,25 @@ function fetchURLforProducts () {
             const productImage = data.products[i].thumbnail;
             const productTitle = data.products[i].title;
             const productDescription = data.products[i].description;
+            const productId = data.products[i].id;
             pageDiv.innerHTML += `
             <div class="card m-2" style="width: 18rem;">
   <img src="${productImage}" class="card-img-top" alt="..." style="aspect-ratio: 16/9; object-fit: cover">
   <div class="card-body">
     <h5 class="card-title">${productTitle}</h5>
     <p class="card-text">${productDescription}</p>
-    <a href="moreinfo.html" class="btn btn-primary">More information</a>
+    <a href="moreinfo.html?id=${productId}" class="btn btn-primary">More information</a>
+    <button onclick="addtoCart('${productTitle}')" id="add-to-cart" class="btn btn-primary mt-2">Add to Cart</button>
   </div>
 </div>
             `
         }
     })
 };
+
+const addtoCart = (title) => {
+ const dropdownItem = document.getElementById("dropdown");
+ dropdownItem.innerHTML += `
+ <li><a class="dropdown-item" href="#">${title}</a></li>
+ `
+}
--- moreinfo.js
+++ moreinfo.js
@@ -1,42 +1,39 @@
-const DUMMY_URL = `https://dummyjson.com/products`;
+const DUMMY_URL = `https://dummyjson.com/products/`;
 const pageDiv = document.getElementById('content-of-page-iphone');
+const params = new URLSearchParams(location.search);
+const id = params.get("id");
 
 fetchURLforProducts();
 
 function fetchURLforProducts () {
-    fetch (DUMMY_URL) 
+    fetch (DUMMY_URL + id) 
     .then (r => r.json())
     .then (data => {
         console.log(data);
         console.log(data.products);
-        for (let i = 0; i < data.products.length; i++) {
-            const productImage = data.products[i].thumbnail;
-            const productTitle = data.products[i].title;
-            const productDescription = data.products[i].description;
-            const morePictures = data.products[i].images;
-            const productPrice = data.products[i].price;
-            const productBrand = data.products[i].brand;
-            const productCategory = data.products[i].category;
-            const productDiscount = data.products[i].discountPercentage;
-            const productRating = data.products[i].rating;
-            const productStock = data.products[i].stock;
+        const {thumbnail, title, description, images, price, brand, category, discountPercentage, rating, stock} = data;
+        let string = "";
+        for (let i = 0 ; i < data.images.length ; i++) {
+          const imgproduct = data.images[i];
+          let imageTag = `<img src="${imgproduct}" class="card-img-top" alt="Product Thumbnail" style="aspect-ratio: 16/9; object-fit: cover">`
+          string += imageTag;
+        }
             pageDiv.innerHTML += `
             <div class="card m-2" style="width: 35.5rem;">
   <div class="card-header d-flex justify-content-between">
-    <span>${productTitle}</span>
-    <span>${productBrand} ${productCategory}</span> 
+    <span>${title}</span>
+    <span>${brand} ${category}</span> 
   </div>
   <ul class="list-group list-group-flush">
-    <li class="list-group-item"><img src="${productImage}" class="card-img-top" alt="Product Thumbnail" style="aspect-ratio: 16/9; object-fit: cover"></li>
-    <li class="list-group-item">$${productPrice} <p>Discount: ${productDiscount}%</p></li>
-    <li class="list-group-item">${productDescription}</li>
-    <li class="list-group-item"></li>
-    <li class="list-group-item">Rating: ${productRating}</li>
-    <li class="list-group-item">In Stock: ${productStock}</li>
-    <li class="list-group-item"><img src="${morePictures}" class="card-img-top" alt="Product Pictures" style="aspect-ratio: 16/9; object-fit: cover"></li>
+    <li class="list-group-item"><img src="${thumbnail}" class="card-img-top" alt="Product Thumbnail" style="aspect-ratio: 16/9; object-fit: cover"></li>
+    <li class="list-group-item">$${price} <p>Discount: ${discountPercentage}%</p></li>
+    <li class="list-group-item">${description}</li>
+    <li class="list-group-item">Rating: ${rating}</li>
+    <li class="list-group-item">In Stock: ${stock}</li>
+    <li class="list-group-item">${string}</li>
   </ul>
 </div>
             `
         }
-    })
+    )
 };
